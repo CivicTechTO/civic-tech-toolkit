bd7a1cb08c6b53cb6b902109c4bf70352afa3057
jordyarms <jordan@addwonder.com>
2024-09-22 21:16:02 -0400

    testing deploy workflow configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://civictechto.github.io',
+  url: 'https://civictechto.github.io/civicsaurus/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/civicsaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
